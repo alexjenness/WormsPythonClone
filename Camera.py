44c3e3b1873d6a24c12ef0961b49561b37b08988
Derek Drost <rlwarrior32@hotmail.com>
2017-04-28 16:01:09 -0500

    Camera change

--- Camera.py
+++ Camera.py
@@ -8,6 +8,7 @@ class Camera:
         self.pos = Vec2d(0,0)
         self.size = size
         self.setCenter(Vec2d(0,0))
+        self.followObject = None
         
     def setCenter(self, pos):
         self.pos.x = pos.x - self.size.x/2
@@ -15,5 +16,13 @@ class Camera:
         
     def setSize(self, size):
         self.size = size
+        
     def getCenter(self):
         return Vec2d(self.pos.x + self.size.x/2, self.pos.y + self.size.y/2)
+        
+    def setFollowObject(self, obj):
+        self.followObject = obj
+        
+    def update(self):
+        if self.followObject != None:
+            self.setCenter(self.followObject.physComp.pos)
